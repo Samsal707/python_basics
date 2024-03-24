6c32d50f03ccb22f7229e5f9c1fc936df0209ef5
Sam Salani <32249487+Samsal707@users.noreply.github.com>
2024-03-24 05:02:13 +0000

    'dynamic typing ...'

--- main.py
+++
@@ -1,8 +0,0 @@
-from hint
-import division
-
-division(10, 2)
-
-division(10, 2.5)
-
-division('Python', 2)
---
+++ scientific_Computing_with_python/main.py
@@ -0,0 +1,7 @@
+from hint import division
+
+division(10, 2)
+
+division(10, 2.5)
+
+division('Python', 2) #this will throw off TypeError: unsupported operand type(s) ...
